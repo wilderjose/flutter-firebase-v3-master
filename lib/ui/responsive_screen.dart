5102c87750ac0223f83c586aec743332993816a7
WilderSuarez <suarezmoraleswilderjose@gmail.com>
2025-10-14 19:04:46 -0600

    fgdtrd

--- lib/ui/responsive_screen.dart
+++ lib/ui/responsive_screen.dart
@@ -16,14 +16,14 @@ class ResponsiveScreen extends StatelessWidget {
           Expanded(
             flex: 2,
             child: Container(
-              color: const Color.fromARGB(255, 30, 7, 160),
+              color: const Color.fromARGB(255, 218, 53, 3),
               child: Center(child: Text('1er expanded')),
             ),
           ),
           Expanded(
             flex: 1,
             child: Container(
-              color: const Color.fromARGB(255, 0, 0, 0),
+              color: const Color.fromARGB(255, 163, 13, 13),
               child: Center(child: Text('2do expanded')),
             ),
           ),
