4b664797948d220cb077fc05d592c350e082dc76
WilderSuarez <suarezmoraleswilderjose@gmail.com>
2025-10-16 21:05:07 -0600

    SCRORE_CREEN

--- lib/ui/scors_screen.dart
+++ lib/ui/scors_screen.dart
@@ -1,27 +1,130 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class ScorsScreen extends StatefulWidget {
-  const ScorsScreen({super.key});
+class ScoreScreen extends StatefulWidget {
+  const ScoreScreen({super.key});
 
   @override
-  State<ScorsScreen> createState() => _ScorsScreenState();
+  State<ScoreScreen> createState() => _ScoreScreenState();
 }
 
-class _ScorsScreenState extends State<ScorsScreen> {
+class _ScoreScreenState extends State<ScoreScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Scores'),
+        title: Text('Puntaje control'),
         actions: [
           IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              // Handle search action
+            onPressed: () async {
+              final name = await showRegisterDialog();
+              log('Nombre recibido $name');
             },
+            icon: Icon(Icons.add),
           ),
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Buscar por nombre',
+                    ),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Card(
+                      child: ListTile(
+                        title: Text('Juan Perez'),
+                        subtitle: Text('3 pts'),
+                        onTap: () {
+                          showBottomSheetScore();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void showBottomSheetScore() {
+    final scores = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Wrap(
+                spacing: 12,
+                children: [
+                  for (final score in scores)
+                    FilledButton(onPressed: () {}, child: Text('+$score')),
+                ],
+              ),
+
+              FilledButton(onPressed: () {}, child: Text('Asignar')),
+              SizedBox(height: 12),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<String> showRegisterDialog() async {
+    final textContoller = TextEditingController();
+
+    final result = await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Registrar'),
+          content: TextField(
+            controller: textContoller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Nombre completo',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, textContoller.text);
+              },
+              child: Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
+    return result;
   }
 }
