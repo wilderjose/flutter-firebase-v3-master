0d7c12e54c546b05bc7729db42a437f6eb8399ba
WilderSuarez <suarezmoraleswilderjose@gmail.com>
2025-10-20 21:01:34 -0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/deshboar/deshboar_screen.dart';
 //import 'package:flutter_application_1/ui/detail_screen.dart';
 //import 'package:flutter_application_1/ui/login_screen.dart';
-import 'package:flutter_application_1/ui/tutoriales/scors_screen.dart';
+//import 'package:flutter_application_1/ui/tutoriales/scors_screen.dart';
 //import 'package:flutter_application_1/ui/login_screen.dart';
 //import 'package:flutter_application_1/ui/responsive_screen.dart';
 //import 'package:flutter_application_1/ui/state_screen.dart';
